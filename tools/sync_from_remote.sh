836a699e98aa944d072a3f36da96ef94335e399a
Moésio Filho <moesio.f@gmail.com>
2025-04-04 22:00:44 -0300

    refactor: explicitly set bind mounts in compose
    - Update radarr, sonarr and qbittorrent docker-composes;
    - Update sync_from_remote to ensure the destination path is clean;

--- tools/sync_from_remote.sh
+++ tools/sync_from_remote.sh
@@ -6,10 +6,11 @@ set -e
 
 # Print help message
 help() {
-	echo "$(basename "$0") [-h] source dest
+	echo "$(basename "$0") [-h] remote prefix dest
 
 ARGUMENTS:
-	source 	root path where the target directories (\`radarr\`, \`jellyfin\`, etc) can be found
+	source 	host machine where the target can be found
+	prefix  prefix (wrt source)where the target directories (\`radarr\`, \`jellyfin\`, etc) can be found
 	dest	destionation path to sync (follows the same structure as the repository)
 
 OPTIONS:
@@ -31,16 +32,21 @@ while getopts ":h" option; do
 done
 
 # Obtain source and dest
-if [[ $# -ne 2 ]]; then
-	echo "Error: missing source and/or dest"
+if [[ $# -ne 3 ]]; then
+	echo "Error: missing source, prefix and/or dest"
 	exit 64
 fi
 source=$1
-dest=$2
+prefix=$2
+dest=$3
 
 # Run rsync
 for t in radarr sonarr prowlarr qbittorrent jellyfin; do
-	rsync -av --exclude="media/*" --exclude="torrent/*" --exclude="torrents/*" $source:$t $dest
+	# Ensure the target path doesn't exist in dest
+	for t1 in config cache theme; do
+		rm -rf $dest/$t1 || true
+	done
+	rsync -av --exclude="media/*" --exclude="torrent/*" --exclude="torrents/*" $source:$prefix/$t $dest
 done
 
 # Wait for all to finish
